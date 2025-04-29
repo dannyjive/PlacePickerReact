17efaa084a242e8960ec8f81d1af9e2d714bf78b
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-04-28 19:15:30 -0700

    added modal timer and progress bar

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,21 @@
-import {useRef } from 'react';
+import {useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
-  dialog.current.showModal();
+  useEffect(() => {
+    if (open){
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
